1c72efdce51e25319a2a56a982756b8f03b4fb4e
andraz87 <aa43120@student.uni-lj.si>
2025-01-09 23:06:03 +0100

    Ustvaril sem dokumentacijo za API.

--- Program.cs
+++ Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ShareCircleDbContext>();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -51,6 +53,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseRouting();
 
